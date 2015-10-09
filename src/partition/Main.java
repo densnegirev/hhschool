8d994833d5566e9655fdf2ef2d10ae6eb1eadada
densnegirev <densnegirev@bk.ru>
2015-10-09 17:51:15 +0300

    Добавил решение второй задачи

---
+++ src/partition/Main.java
@@ -0,0 +1,29 @@
+package partition;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static int partition(int n, int k) {
+        if (n < k) {
+            return 0;
+        }
+        else if (k == 1 || n == k) {
+            return 1;
+        }
+        else {
+            return partition(n-1, k-1) + partition(n-k, k);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = reader.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+
+            System.out.println(partition(n, k));
+        }
+    }
+
+
